c77129f44049530ee85af882a277a760d3cdfb81
Ariel Piccolo <47399439+arielpiccolo@users.noreply.github.com>
2021-04-13 10:26:50 +0100

    two new scripts added

---
+++ removeOldLogs.ps1
@@ -0,0 +1,6 @@
+﻿$logTarget = 'C:\Windows\Temp'
+$logAge = '-30'
+
+Get-ChildItem "*.log" –Path "$logTarget" -Recurse | Where-Object {($_.LastWriteTime -lt (Get-Date).AddDays($logAge))} | Remove-Item 
+
+
---
+++ restartSrv.ps1
@@ -0,0 +1,11 @@
+﻿#Clear-Host
+$SrvName = "PhoneSvc"
+#Write-Host *** Restarting $SrvName Service *** 
+Start-Sleep -Seconds 2
+Restart-Service $SrvName
+#Stop-Service $SrvName
+#Start-Service $SrvName
+#Get-Service $SrvName
+#Write-Host " "
+#Read-Host "Press any key to exit "
+Exit
